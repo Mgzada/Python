a817cbd130286f01b3f676f12417af284b712df1
Messias Gabriel <158516176+Mgzada@users.noreply.github.com>
2024-08-28 17:08:15 -0300

    Add files via upload

---
+++ 01-aula.py
@@ -0,0 +1,20 @@
+ # Aqui eu posso adicionar comentários
+ # Aqui eu posso adicionar comentários
+print("Messias") # comentári a linha do código
+print('Messias')
+print('Messias Gabriel')
+print('Messias "MEME" Gabriel')
+print("Messias \"MEME\" Gabriel")
+      
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False 
+print(teste)
+print(type(teste))
